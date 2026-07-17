c57d3f66b4f17e1b2f8a1491066aa292da400fc6
TakWolf <takwolf@foxmail.com>
2026-07-17 17:15:37 +0800

    命名调整

--- tools/update.py
+++ tools/update.py
@@ -4,7 +4,7 @@ from tools import project_root_dir
 
 
 def main():
-    hash_name = '6d1e7d96f0ad3f253d961e52922a2b15b95f853d'
+    sha = '6d1e7d96f0ad3f253d961e52922a2b15b95f853d'
     src_root_dir = project_root_dir.joinpath('bitsnpicas', 'src', 'main', 'java', 'com', 'kreative', 'bitsnpicas')
     for file_dir, _, file_names in src_root_dir.walk():
         for file_name in file_names:
@@ -12,7 +12,7 @@ def main():
                 continue
 
             file_path = file_dir.joinpath(file_name)
-            url = f'https://raw.githubusercontent.com/kreativekorp/bitsnpicas/{hash_name}/main/java/BitsNPicas/src/com/kreative/bitsnpicas/{file_path.relative_to(src_root_dir)}'.replace('\\', '/')
+            url = f'https://raw.githubusercontent.com/kreativekorp/bitsnpicas/{sha}/main/java/BitsNPicas/src/com/kreative/bitsnpicas/{file_path.relative_to(src_root_dir)}'.replace('\\', '/')
             response = httpx.get(url)
             assert response.is_success and 'text/plain' in response.headers['Content-Type']
 
